27f62bdd3318cfb925b3b3df81e3176777819f9f
unknown <sojibjubayer@gmail.com>
2023-10-19 11:54:32 +0600

    delete functionality added to myCart page

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,17 +1,57 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 import { Helmet } from "react-helmet";
+import Swal from "sweetalert2";
 
 
 const MyCart = () => {
-    const cartResult=useLoaderData()
-    const firebaseUser=useContext(AuthContext)
+    const cartResult = useLoaderData()
+    console.log(cartResult);
+    const{cartProduct,setCartProduct}=useState(cartResult)
+    const firebaseUser = useContext(AuthContext)
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+               
+                console.log('deleted');
+
+                fetch(`http://localhost:5000/cart/${_id}`,{
+                    method:'DELETE',
+                })
+                .then(res=>res.json())
+                .then(data=>{
+                    console.log(data);
+                    if(data.deletedCount>0){
+                         Swal.fire(
+                    'Deleted!',
+                    'Your product has been deleted.',
+                    'success'
+                )
+                // filter from cart products and set it to remaining cart product
+                const remaining=cartProduct.filter(product=>product._id != _id)
+                setCartProduct(remaining)
+                    }
+                })
+            }
+        })
+    }
 
 
     return (
         <div>
-          
+
             <div className="min-h-screen">
                 <div>
                     <h2 className="md:w-[450px] mx-auto font-bold p-2 rounded-sm text-center text-2xl bg-black text-red-400  ">
@@ -20,7 +60,7 @@ const MyCart = () => {
 
                 <div className='grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5 py-7'>
                     {
-                        cartResult.filter(target=>target.email==firebaseUser.user.email).map(brand => <div key={brand.id}>
+                        cartResult.filter(target => target.email == firebaseUser.user.email).map(brand => <div key={brand.id}>
                             <div className="md:w-[350px] h-[400px] mx-auto bg-amber-200 rounded-t-xl">
                                 <img className="w-full h-[230px] rounded-t-xl" src={brand.image} alt="" />
                                 <h3 className=" text-center text-xl font-bold pt-2">{brand.name}</h3>
@@ -30,10 +70,11 @@ const MyCart = () => {
                                     <h3 className="text-center text-base font-bold pt-2">Rating: {brand.rating}</h3>
                                 </div>
                                 <div className="flex  justify-center mt-5">
-                                    
-                                        <button  className="bg-[#FFA171] hover:bg-green-400 btn btn-sm mr-5">Add to Cart</button>
-                                    
-                                        
+
+                                    <button
+                                        onClick={() => handleDelete(brand._id)} className="bg-green-400 hover:bg-red-500 btn btn-sm mr-5">Delete from cart</button>
+
+
                                 </div>
                             </div>
                         </div>)
@@ -48,7 +89,7 @@ const MyCart = () => {
 
         </div>
 
-       
+
     );
 };
 
